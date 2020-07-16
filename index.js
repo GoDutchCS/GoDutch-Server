6bb496a25c5f0407c73709c825c938f4696fd0e4
pacokwon <haechank@gmail.com>
2020-07-16 16:47:07 +0000

    add: bind to database using mongoose

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const http = require('http')
 
 const app = express()
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongodb connection error:'));
+db.once('open', () => console.log('connected to mongodb'));
+mongoose.connect('mongodb://localhost/godutch', { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true });
 
 app.set('port', process.env.PORT || 3000)
 app.use(express.urlencoded({ extended: true }))
