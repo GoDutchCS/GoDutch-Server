1607085465a2f176bcd9ae743bf4ca7d4eaef736
pacokwon <haechank@gmail.com>
2020-07-16 18:52:42 +0000

    add: use the defined routers as endpoints

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import http from 'http'
+import { api, auth } from './api/index.js'
 
 const app = express()
 
@@ -12,10 +13,9 @@ mongoose.connect('mongodb://localhost/godutch', { useNewUrlParser: true, useFind
 app.set('port', process.env.PORT || 3000)
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
-app.get('/', (req, res) => {
-    res.json({})
-})
+app.get('/', (req, res) => { res.json({}) })
+app.use('/api', api)
+app.use('/auth', auth)
 
 http.createServer(app).listen(app.get('port'), () => {
     console.log(`Express server listening on port ${app.get('port')}`)
