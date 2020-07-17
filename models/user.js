c90b2cc859767e9a07e265eb2165598831dcc385
pacokwon <haechank@gmail.com>
2020-07-17 16:38:13 +0900

    add: user schema

---
+++ models/user.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema({
+    id: String,
+    first_name: String,
+    last_name: String,
+    email: String,
+    account_number: String,
+}, {
+    collection: 'users'
+})
+
+export default mongoose.model('User', userSchema)
