6a1b83bda61454bdc49dc859627d00e6e1b6172f
pacokwon <haechank@gmail.com>
2020-07-18 05:37:35 +0000

    add: use images router from api

--- api/api.js
+++ api/api.js
@@ -1,6 +1,9 @@
 import express from 'express'
+import imagesRouter from './images.js'
 import Contact from '../models/contact.js'
 const router = express.Router()
+
+router.use('/images', imagesRouter)
 
 router.post('/contacts/insert', async (req, res) => {
     const newContact = new Contact(req.body)
