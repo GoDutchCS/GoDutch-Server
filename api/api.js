9fba1ea38a781c557431d8b17265a414f9e1e2b1
pacokwon <haechank@gmail.com>
2020-07-18 14:10:43 +0900

    update: add to user's contact if user exists

--- api/api.js
+++ api/api.js
@@ -3,13 +3,27 @@ import Contact from '../models/contact.js'
 const router = express.Router()
 
 router.post('/contacts/insert', async (req, res) => {
-    const newContact = new Contact(req.body)
-    try {
-        await newContact.save()
-    } catch (err) {
-        res.status(500).send(err)
+    const exists = await Contact.exists({ id: req.body.id })
+
+    if (exists) {
+        try {
+            await Contact.update(
+                { id: req.body.id },
+                { $push: { contacts: req.body.contacts } }
+            )
+        } catch (err) {
+            res.status(500).send(err)
+        }
+        res.json({ success: true })
+    } else {
+        const newContact = new Contact(req.body)
+        try {
+            await newContact.save()
+        } catch (err) {
+            res.status(500).send(err)
+        }
+        res.json({ success: true })
     }
-    res.json({ success: true })
 })
 
 export default router
