546ac15c4817e13b5e4b79a8d6d34d4949f6ee88
pacokwon <haechank@gmail.com>
2020-07-20 00:38:51 +0900

    add: add party endpoint

--- api/parties.js
+++ api/parties.js
@@ -3,7 +3,26 @@ import Party from '../models/party.js'
 const router = express.Router()
 
 router.post('/add', async (req, res) => {
+    let { id, members } = req.body
 
+    if (id === "") {
+        try {
+            const count = await Party.count({}) + 1
+            id = `Party ${count.toString()}`
+        } catch (err) {
+            return res.status(500).send(err)
+        }
+    }
+
+    const newParty = new Party({ id, members, transactions: [] })
+
+    try {
+        await newParty.save()
+    } catch (err) {
+        return res.status(500).send(err)
+    }
+
+    res.json({ success: true })
 })
 
 export default router
