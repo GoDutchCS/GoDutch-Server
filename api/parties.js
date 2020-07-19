0a4f5c5b5ffd7573c88582d8816711ace1d0682a
pacokwon <haechank@gmail.com>
2020-07-19 23:19:26 +0900

    add: skeleton schema and add endpoint for party

---
+++ api/parties.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import Party from '../models/party.js'
+const router = express.Router()
+
+router.post('/add', async (req, res) => {
+
+})
+
+export default router
---
+++ models/party.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose'
+
+const Transaction = {
+    participants: {
+        type: [String] ,
+        required: true
+    },
+    cashflow: {
+        from: {
+            type: String,
+            required: true
+        },
+        to: {
+            type: String,
+            required: true
+        },
+        amount: {
+            type: Number,
+            required: true
+        }
+    }
+}
+
+const partySchema = mongoose.Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    members: {
+        type: [String] ,
+        required: true
+    },
+    transactions: {
+        type: [
+            Transaction
+        ],
+        required: true
+    }
+}, {
+    collection: 'parties'
+})
+
+export default mongoose.model('Party', partySchema)
